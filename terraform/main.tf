8ada46d447061b4cd942100f55a015159e1f116e
Andrii Moskovets <andor2001@ukr.net>
2025-04-15 16:42:31 +0300

    get ami id

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,17 +11,6 @@ provider "aws" {
   region = "eu-west-2" # London
 }
 
-# # create instance Web sever 1
-# resource "aws_instance" "WebSRV-1" {
-#     ami = var.ami_id
-#     instance_type = var.inst_type
-    
-#     tags = {
-#       Name = "WebSRV-1"
-#     }
-  
-# }
-
 resource "aws_instance" "web" {
   ami = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
