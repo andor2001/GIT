875980d93f86f34b649d392efe67ff457a19cbb6
Andrii Moskovets <andor2001@ukr.net>
2025-04-15 16:42:09 +0300

    get ami id

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,13 +11,22 @@ provider "aws" {
   region = "eu-west-2" # London
 }
 
-# create instance Web sever 1
-resource "aws_instance" "WebSRV-1" {
-    ami = var.ami_id
-    instance_type = "t2.micro"
-
-    tags = {
-      Name = "WebSRV-1"
-    }
+# # create instance Web sever 1
+# resource "aws_instance" "WebSRV-1" {
+#     ami = var.ami_id
+#     instance_type = var.inst_type
+    
+#     tags = {
+#       Name = "WebSRV-1"
+#     }
   
+# }
+
+resource "aws_instance" "web" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Hello web!"
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,25 @@ variable "ami_id" {
     description = "The id of the machine image (AMI) to use for the server"
     default = "ami-04da26f654d3383cf"
 }
+
+variable "inst_type" {
+  type = string
+  description = "This is instance type for the server"
+  default = "t2.micro"
+}
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+      name = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+
+    filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical  
+}
