11d3453ed2fc7149049d9e36695db7330336516c
Andrii Moskovets <andor2001@ukr.net>
2025-04-15 16:14:45 +0300

    correct file main.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "ami_id" {
     type = string
     description = "The id of the machine image (AMI) to use for the server"
-    default = ["ami-04da26f654d3383cf"]
+    default = "ami-04da26f654d3383cf"
 }
