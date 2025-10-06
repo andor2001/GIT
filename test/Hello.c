ebbfc8f1cb63798cdf9cfe3176dbb6e59abf50e8
Andrii Moskovets <andor2001@ukr.net>
2025-10-06 11:38:38 +0300

    Test java

---
+++ test/Hello.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main() {
+    int anb = 1;
+        while (anb < 10) {
+        
+            printf("Hello World!\n");
+            anb++;
+        
+    return 0;
+    }
+}
---
+++ test/loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+int i = 0;
+
+while (i < 5) {
+  i++;
+  printf("Hello World!\n%d\t", i);
+}
+  
+  return 0;
+}
